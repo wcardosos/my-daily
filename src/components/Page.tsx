6fe238464e51260ef7f0f2709d09339d71085019
Wagner Cardoso <wagnerdev01@gmail.com>
2022-04-05 19:52:35 -0300

    feat(components) Add page component

---
+++ src/components/Page.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import {
+  Flex,
+} from '@chakra-ui/react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import { useResponsiveness } from '../hooks/useResponsiveness';
+
+interface IPageProps {
+  children: ReactNode
+}
+
+export default function Page({ children }: IPageProps) {
+  const isMobileVersion = useResponsiveness('mobile');
+  return (
+    <Flex direction="column">
+      <Header />
+      <Flex pt="8" px={isMobileVersion ? '6' : 0} w="100%" maxWidth={1480} mx="auto">
+        {!isMobileVersion && <Sidebar />}
+        {children}
+      </Flex>
+    </Flex>
+  );
+}
