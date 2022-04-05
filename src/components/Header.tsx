9634f9390602e5c40fc119f0585095baf211be02
Wagner Cardoso <wagnerdev01@gmail.com>
2022-04-05 14:41:44 -0300

    feat(components/Header) Add profile, action bar and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,40 @@
 import {
+  Center,
+  Divider,
   Flex,
   Spacer,
 } from '@chakra-ui/react';
 import React from 'react';
 import Logo from './Logo';
 import Profile from './Profile';
+import ActionBar from './ActionBar';
+import { useResponsiveness } from '../hooks/useResponsiveness';
 
 export default function Header() {
+  const isDesktopVersion = useResponsiveness('desktop');
+
+  const handleLogout = () => console.log('Logout'); // eslint-disable-line no-console
+
   return (
-    <Flex px="32" align="center" h="10vh">
+    <Flex
+      direction={isDesktopVersion ? 'row' : 'row-reverse'}
+      pl={['6', '8']}
+      pr={['6', '32']}
+      align="center"
+      h="10vh"
+    >
       <Logo />
       <Spacer />
       <Flex>
-        <Profile email="wagner@email.com" name="Wagner Cardoso" />
+        { isDesktopVersion && (
+          <>
+            <Profile email="wagner@email.com" name="Wagner Cardoso" />
+            <Center px="4">
+              <Divider borderColor="gray.500" orientation="vertical" />
+            </Center>
+            <ActionBar handleLogout={handleLogout} />
+          </>
+        )}
       </Flex>
     </Flex>
   );
--- tests/components/Header.spec.tsx
+++ tests/components/Header.spec.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react';
+import { useResponsiveness } from '../../src/hooks/useResponsiveness';
 import Header from '../../src/components/Header';
 import '@testing-library/jest-dom/extend-expect';
 
-describe('Component: Header', () => {
-  beforeEach(() => {
-    render(<Header />);
-  });
+jest.mock('../../src/hooks/useResponsiveness');
 
+describe('Component: Header', () => {
   afterEach(() => {
     cleanup();
   });
 
-  it('Should render the header', () => {
-    expect(screen.getByTestId('logo')).toBeInTheDocument();
-    expect(screen.getByTestId('profile')).toBeInTheDocument();
+  describe('Responsiveness', () => {
+    describe('Desktop', () => {
+      it('Should render the logo, profile, and action bar ', () => {
+        const useResponsivenessMock = useResponsiveness as jest
+        .MockedFunction<typeof useResponsiveness>;
+
+        useResponsivenessMock.mockReturnValue(true);
+
+        render(<Header />);
+
+        expect(useResponsivenessMock).toHaveBeenCalled();
+        expect(screen.getByTestId('logo')).toBeInTheDocument();
+        expect(screen.getByTestId('profile')).toBeInTheDocument();
+        expect(screen.getByTestId('action-bar')).toBeInTheDocument();
+      });
+    });
+
+    describe('Mobile', () => {
+      it('Should render the logo; does not render profile, and action bar ', () => {
+        const useResponsivenessMock = useResponsiveness as jest
+        .MockedFunction<typeof useResponsiveness>;
+
+        useResponsivenessMock.mockReturnValue(false);
+
+        render(<Header />);
+
+        expect(useResponsivenessMock).toHaveBeenCalled();
+        expect(screen.getByTestId('logo')).toBeInTheDocument();
+        expect(screen.queryByTestId('profile')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('action-bar')).not.toBeInTheDocument();
+      });
+    });
   });
 });
