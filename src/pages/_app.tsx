146af2d39b506c338b92057763e92e79ad43ffaa
Wagner Cardoso <wagnerdev01@gmail.com>
2022-04-06 15:04:13 -0300

    feat(pages/_app) Add daily work provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme';
+import { DailyWorkProvider } from '../contexts/DailyWorkContext';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <DailyWorkProvider>
+        <Component {...pageProps} />
+      </DailyWorkProvider>
     </ChakraProvider>
   );
 }
