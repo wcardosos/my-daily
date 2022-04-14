6eab04e8bacf1e4051bbd8485c609c05e1a9d53e
Wagner Cardoso <wagnerdev01@gmail.com>
2022-04-14 05:21:21 -0300

    feat(repositories) Add daily prisma postgres

---
+++ tests/server/repositories/Daily/DailyPrismaPostgresRepository.spec.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import { DateHandler } from '../../../../src/server/providers/DateHandler';
+import { DailyPrismaPostgresRepository } from '../../../../src/server/repositories/Daily/DailyPrismaPostgresRepository';
+
+describe('Repository: DailyPrismaPostgres', () => {
+  const prismaClientMock = {} as jest.MockedObject<PrismaClient>;
+  const dailyPrismaMock = {
+    create: jest.fn(),
+    findUnique: jest.fn(),
+  };
+  const getTodayDateHandlerSpy = jest.spyOn(DateHandler, 'getToday');
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  prismaClientMock.daily = dailyPrismaMock as any;
+
+  const dailyPrismaPostgresRepository = new DailyPrismaPostgresRepository(prismaClientMock);
+
+  describe('getToday', () => {
+    getTodayDateHandlerSpy.mockReturnValue('id');
+
+    it('Should return the id when the daily exists', async() => {
+      dailyPrismaMock.findUnique.mockResolvedValue({ id: 'daily id' });
+
+      const result = await dailyPrismaPostgresRepository.getToday();
+
+      expect(dailyPrismaMock.findUnique).toHaveBeenCalledWith({ where: { id: 'id' } });
+      expect(result).toBe('daily id');
+    });
+
+    it('Should return null when the daily not exists', async() => {
+      dailyPrismaMock.findUnique.mockResolvedValue(null);
+
+      const result = await dailyPrismaPostgresRepository.getToday();
+
+      expect(dailyPrismaMock.findUnique).toHaveBeenCalledWith({ where: { id: 'id' } });
+      expect(result).toBeNull();
+    });
+  });
+});
