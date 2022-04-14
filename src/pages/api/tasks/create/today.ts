578075e39844be59be557dec3bab4f864ddf3baf
Wagner Cardoso <wagnerdev01@gmail.com>
2022-04-14 05:48:00 -0300

    feat(api/tasks) Add create routes

--- src/pages/api/tasks/create/today.ts
+++ src/pages/api/tasks/create/today.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { TasksController } from '../../../server/controllers/TasksController';
+import { TasksController } from '../../../../server/controllers/TasksController';
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ): Promise<NextApiResponse | void> {
   if (request.method === 'POST') {
-    return TasksController.create(request, response);
+    return TasksController.createToday(request, response);
   }
 
   return response.status(405).end();
