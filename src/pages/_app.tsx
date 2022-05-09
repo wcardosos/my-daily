df115acd162a8359e3ac4f4c9b2fbbdee580f5e0
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-09 18:08:28 -0300

    feat(app) Add the next auth library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { QueryClientProvider } from 'react-query';
 import { AppProps } from 'next/app';
+import { QueryClientProvider } from 'react-query';
 import { ChakraProvider } from '@chakra-ui/react';
+import { SessionProvider } from 'next-auth/react';
 import { queryClient } from '../lib/react-query/queryClient';
 import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps): React.ReactNode {
   return (
     <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <SessionProvider session={session}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </SessionProvider>
     </ChakraProvider>
   );
 }
