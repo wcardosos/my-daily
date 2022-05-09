b0446f0d6bf688a662fb9499004054aeb3a9b314
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-09 18:14:12 -0300

    feat(repositories) Add users repository

---
+++ src/server/repositories/User/UserPrismaPostgresRepository.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { PrismaClient, User as UserPrisma } from '@prisma/client';
+import { User } from '../../entities/User';
+
+export class UserPrismaPostgresRepository {
+  private client: PrismaClient;
+
+  constructor(
+    client: PrismaClient,
+  ) {
+    this.client = client;
+  }
+
+  public async getByEmail(email: string): Promise<UserPrisma> {
+    const user = await this.client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+
+  public async save(user: User): Promise<UserPrisma> {
+    const {
+      email,
+      name,
+      pictureUrl,
+    } = user.get();
+
+    return this.client.user.create({
+      data: {
+        email,
+        name,
+        picture_url: pictureUrl,
+        provider: 'github',
+      },
+    });
+  }
+}
