a3c152b27a31e67bdee33f221f45b75ae5d6b0df
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-12 09:44:19 -0300

    feat(pages) Add pages title

--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -12,6 +12,7 @@ import {
   Heading,
   Text,
 } from '@chakra-ui/react';
+import Head from 'next/head';
 import axios from 'axios';
 import Page from '../components/Page';
 import Button from '../components/Button';
@@ -47,127 +48,132 @@ export default function Home({ user: { email } }: IConfigProps) {
   };
 
   return (
-    <Page>
-      <Box w="100%">
-        <Heading color="purple.700">Configurações</Heading>
-        <Box>
-          <Box pt={['8', '16']}>
-            <Heading size="sm" color="purple.700">Sua conta</Heading>
-            <Text pt={['6', '8']}>
-              Sua conta está linkada com o
-              <strong> Github</strong>
-              .
-            </Text>
-          </Box>
-          <Box pt={['8', '16']}>
-            <Heading size="sm" color="purple.700">Zona de perigo</Heading>
-            <Box pt={['6', '8']} pb="4">
-              <Button
-                text="Excluir dados"
-                size="normal"
-                onClick={onClickDeleteData}
-                bgColor="red.500"
-                colorScheme="red"
-              />
-              <Text
-                pt="2"
-                fontSize="sm"
-                color="gray.500"
-              >
-                Esta ação irá excluir permanentemente todos os dados da sua daily.
+    <>
+      <Head>
+        <title>Configurações - myDaily</title>
+      </Head>
+      <Page>
+        <Box w="100%">
+          <Heading color="purple.700">Configurações</Heading>
+          <Box>
+            <Box pt={['8', '16']}>
+              <Heading size="sm" color="purple.700">Sua conta</Heading>
+              <Text pt={['6', '8']}>
+                Sua conta está linkada com o
+                <strong> Github</strong>
+                .
               </Text>
             </Box>
-            <Box pb="4">
-              <Button
-                text="Excluir conta"
-                size="normal"
-                onClick={onClickDeleteAccount}
-                bgColor="red.500"
-                colorScheme="red"
-              />
-              <Text
-                pt="2"
-                fontSize="sm"
-                color="gray.500"
-              >
-                Esta ação irá excluir permanentemente os dados da sua conta no myDaily.
-              </Text>
+            <Box pt={['8', '16']}>
+              <Heading size="sm" color="purple.700">Zona de perigo</Heading>
+              <Box pt={['6', '8']} pb="4">
+                <Button
+                  text="Excluir dados"
+                  size="normal"
+                  onClick={onClickDeleteData}
+                  bgColor="red.500"
+                  colorScheme="red"
+                />
+                <Text
+                  pt="2"
+                  fontSize="sm"
+                  color="gray.500"
+                >
+                  Esta ação irá excluir permanentemente todos os dados da sua daily.
+                </Text>
+              </Box>
+              <Box pb="4">
+                <Button
+                  text="Excluir conta"
+                  size="normal"
+                  onClick={onClickDeleteAccount}
+                  bgColor="red.500"
+                  colorScheme="red"
+                />
+                <Text
+                  pt="2"
+                  fontSize="sm"
+                  color="gray.500"
+                >
+                  Esta ação irá excluir permanentemente os dados da sua conta no myDaily.
+                </Text>
+              </Box>
             </Box>
           </Box>
         </Box>
-      </Box>
-      <AlertDialog
-        isOpen={isToShowDeleteDataAlert}
-        leastDestructiveRef={cancelRef}
-        onClose={onClickCloseDeleteDataAlert}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold" color="purple.700">
-              Deletar dados da conta
-            </AlertDialogHeader>
+        <AlertDialog
+          isOpen={isToShowDeleteDataAlert}
+          leastDestructiveRef={cancelRef}
+          onClose={onClickCloseDeleteDataAlert}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold" color="purple.700">
+                Deletar dados da conta
+              </AlertDialogHeader>
 
-            <AlertDialogBody>
-              Você tem certeza ? Você não poderá recuperar os dados no futuro.
-            </AlertDialogBody>
+              <AlertDialogBody>
+                Você tem certeza ? Você não poderá recuperar os dados no futuro.
+              </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button
-                text="Cancelar"
-                size="normal"
-                onClick={onClickCloseDeleteDataAlert}
-                bgColor="gray.300"
-                colorScheme="gray"
-              />
-              <Button
-                text="Apagar dados"
-                size="normal"
-                onClick={deleteData}
-                colorScheme="red"
-                bgColor="red.500"
-                ml={3}
-              />
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-      <AlertDialog
-        isOpen={isToShowDeleteAccountAlert}
-        leastDestructiveRef={cancelRef}
-        onClose={onClickCloseDeleteAccountAlert}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold" color="purple.700">
-              Deletar conta
-            </AlertDialogHeader>
+              <AlertDialogFooter>
+                <Button
+                  text="Cancelar"
+                  size="normal"
+                  onClick={onClickCloseDeleteDataAlert}
+                  bgColor="gray.300"
+                  colorScheme="gray"
+                />
+                <Button
+                  text="Apagar dados"
+                  size="normal"
+                  onClick={deleteData}
+                  colorScheme="red"
+                  bgColor="red.500"
+                  ml={3}
+                />
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+        <AlertDialog
+          isOpen={isToShowDeleteAccountAlert}
+          leastDestructiveRef={cancelRef}
+          onClose={onClickCloseDeleteAccountAlert}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold" color="purple.700">
+                Deletar conta
+              </AlertDialogHeader>
 
-            <AlertDialogBody>
-              {/* eslint-disable-next-line max-len */}
-              Você tem certeza ? Será necessário criar uma nova conta para acessar o myDaily novamente.
-            </AlertDialogBody>
+              <AlertDialogBody>
+                {/* eslint-disable-next-line max-len */}
+                Você tem certeza ? Será necessário criar uma nova conta para acessar o myDaily novamente.
+              </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button
-                text="Cancelar"
-                size="normal"
-                onClick={onClickCloseDeleteAccountAlert}
-                bgColor="gray.300"
-                colorScheme="gray"
-              />
-              <Button
-                text="Apagar conta"
-                size="normal"
-                onClick={deleteAccount}
-                colorScheme="red"
-                bgColor="red.500"
-                ml={3}
-              />
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </Page>
+              <AlertDialogFooter>
+                <Button
+                  text="Cancelar"
+                  size="normal"
+                  onClick={onClickCloseDeleteAccountAlert}
+                  bgColor="gray.300"
+                  colorScheme="gray"
+                />
+                <Button
+                  text="Apagar conta"
+                  size="normal"
+                  onClick={deleteAccount}
+                  colorScheme="red"
+                  bgColor="red.500"
+                  ml={3}
+                />
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </Page>
+    </>
   );
 }
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,6 +10,7 @@ import {
 import { getSession, signIn } from 'next-auth/react';
 import { GetServerSideProps } from 'next';
 import { FaGithub } from 'react-icons/fa';
+import Head from 'next/head';
 import Logo from '../../components/Logo';
 import AuthSectionInfo from '../../components/AuthSectionInfo';
 
@@ -19,44 +20,49 @@ export default function Login() {
   };
 
   return (
-    <Box w="100%">
-      <Stack
-        w="100%"
-        direction={['column', 'row']}
-      >
-        <Box
-          w={['100%', '100%', '50%']}
-          h="100vh"
-          px={['6', '16']}
-          py={['16', '8']}
+    <>
+      <Head>
+        <title>Login - myDaily</title>
+      </Head>
+      <Box w="100%">
+        <Stack
+          w="100%"
+          direction={['column', 'row']}
         >
-          <Center>
-            <Logo />
-          </Center>
-          <Box py={['16', '8']}>
-            <Text fontSize="32">Bem vindo!</Text>
-            <Text color="gray.600">Faça seu login</Text>
+          <Box
+            w={['100%', '100%', '50%']}
+            h="100vh"
+            px={['6', '16']}
+            py={['16', '8']}
+          >
+            <Center>
+              <Logo />
+            </Center>
+            <Box py={['16', '8']}>
+              <Text fontSize="32">Bem vindo!</Text>
+              <Text color="gray.600">Faça seu login</Text>
+            </Box>
+            <Box>
+              <Button
+                bgColor="gray.800"
+                color="gray.50"
+                fontSize="12px"
+                fontWeight={400}
+                h="48px"
+                rightIcon={<FaGithub fontSize={24} />}
+                onClick={handleLogin}
+                _hover={{
+                  bgColor: 'gray.700',
+                }}
+              >
+                Entrar com Github
+              </Button>
+            </Box>
           </Box>
-          <Box>
-            <Button
-              bgColor="gray.800"
-              color="gray.50"
-              fontSize="12px"
-              fontWeight={400}
-              h="48px"
-              rightIcon={<FaGithub fontSize={24} />}
-              onClick={handleLogin}
-              _hover={{
-                bgColor: 'gray.700',
-              }}
-            >
-              Entrar com Github
-            </Button>
-          </Box>
-        </Box>
-        <AuthSectionInfo />
-      </Stack>
-    </Box>
+          <AuthSectionInfo />
+        </Stack>
+      </Box>
+    </>
   );
 }
 
